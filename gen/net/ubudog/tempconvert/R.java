06e180e4fbb140571e7eed823f7f49e48550172d
ubudog <linuxdude@ubuntu.com>
2013-01-06 20:25:09 -0500

    Adds an app menu.

--- gen/net/ubudog/tempconvert/R.java
+++ gen/net/ubudog/tempconvert/R.java
@@ -14,14 +14,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int calculate=0x7f050004;
-        public static final int celsius=0x7f050002;
-        public static final int fahrenheit=0x7f050001;
-        public static final int tempgroup=0x7f050000;
-        public static final int value=0x7f050003;
+        public static final int calculate=0x7f060004;
+        public static final int celsius=0x7f060002;
+        public static final int fahrenheit=0x7f060001;
+        public static final int tempgroup=0x7f060000;
+        public static final int value=0x7f060003;
+        public static final int version=0x7f060005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+    }
+    public static final class menu {
+        public static final int menu=0x7f050000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/net/ubudog/tempconvert/TempConvert.java
+++ src/net/ubudog/tempconvert/TempConvert.java
@@ -1,12 +1,22 @@
 package net.ubudog.tempconvert;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class TempConvert extends Activity {
@@ -22,7 +32,6 @@ public class TempConvert extends Activity {
     }
     
     public void main() { 
-   //  	final RadioGroup radgroup = (RadioGroup) findViewById(R.id.tempgroup); 
     	final RadioButton fahrenheit = (RadioButton) findViewById(R.id.fahrenheit); 
     	final RadioButton celsius = (RadioButton) findViewById(R.id.celsius);
     	    	
@@ -73,9 +82,45 @@ public class TempConvert extends Activity {
     		Toast resultToastC = Toast.makeText(getApplicationContext(), "Result: " + result, Toast.LENGTH_LONG); 
     	//	 °C  x  9/5 + 32 = °F
     		resultToastC.show(); 
+    	} else { 
+    		Toast modeError = Toast.makeText(getApplicationContext(), "Not a valid mode.", Toast.LENGTH_LONG); 
+    		modeError.show(); 
     	}
     }
     
+    public void versionInfo() { 
+        AlertDialog.Builder alertbox = new AlertDialog.Builder(this);
+        
+        alertbox.setTitle("Version"); 
+        alertbox.setMessage("TempConvert Version 1.0\nSource code: \nhttp://github.com/ubudog/tempconvert");
+       
+        alertbox.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+            
+            public void onClick(DialogInterface arg0, int arg1) {
+            		
+            }
+        });
+        
+        alertbox.show();
+    }
     
-    
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+    	super.onCreateOptionsMenu(menu);
+    	MenuInflater awesome = getMenuInflater();
+    	awesome.inflate(R.menu.menu, menu);
+    	return true;
+    	
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	
+    	switch (item.getItemId()) {
+    	case R.id.version:
+    		versionInfo(); 
+    		return true; 
+    	}
+    	
+    	return false;
+    }; 
 }
